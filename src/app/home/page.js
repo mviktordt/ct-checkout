7510d0c09bde1b6e3257da77dd2704dfe237622a
Viktor Marcsenko <vkma@mms-t-systems.de>
2023-12-04 14:19:25 +0100

    added cart find logic with filtering by customer id

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,103 +1,20 @@
-"use client";
+import React from 'react';
+import dynamic from 'next/dynamic';
 
-import React, { useEffect, useState, useRef } from 'react';
-import { checkout, init } from '@commercetools/checkout-browser-sdk';
-import  {
-    obtainAccessToken,
-    checkoutConfig,
-    getCartByUserId
-  } from '../ct-client';
-
+const CheckoutComponentWithNoSSR = dynamic(
+    () => import('./CheckoutComponent'),
+    { ssr: false }
+);
 
 const Home = () => {
 
-  const [accessToken, setAccessToken] = useState(null);
-  const [cartId, setCartId] = useState("not able to fetch by customer ID");
-  const [customerId, setCustomerId] = useState(null);
-  const isMounted = useRef(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = await obtainAccessToken();
-        setAccessToken(token.access_token);
-        const customerIdExtr = extractCustomerIdFromScope(token.scope);
-        setCustomerId(customerIdExtr);
-        console.log('the extrected customer id is: ', customerIdExtr);
-
-        //TODO - An active cart for the customer ****** does not exist.
-        // getCartByUserId(customerIdExtr).then(cartId=>{
-        //   console.log('cart id is: ', cartId);
-        // })
-      } catch (error) {
-        console.error('Error fetching token:', error);
-      }
-    };
-
-    if (!isMounted.current) {
-      isMounted.current = true;
-      if (accessToken === null) {
-        fetchData();
-      }
-    }
-  }, [accessToken]);
-
-  const extractCustomerIdFromScope = (inputString) => {
-    const regex = /customer_id:([\w-]+)/;
-    const match = inputString.match(regex);
-  
-    if (match && match[1]) {
-      return match[1];
-    } else {
-      console.error('Customer_id not found in the input string');
-      return null;
-    }
-    }
-
-  const handleCheckout = () => {
-    init({
-      checkoutConfig
-  });
-    checkout({
-      cartId,
-      accessToken,
-    });
-  };
-
-
-  return (
-    customerId&&cartId&&accessToken&&<>
-      <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        background: 'linear-gradient(to right, #2c3e50, #3498db)', // Gradient background
-      }}
-    >
-      <h1 style={{ color: '#fff' }}>Access Token: {accessToken}</h1>
-      <h1 style={{ color: '#fff' }}>Customer Id: {customerId}</h1>
-      <h2 style={{ color: '#fff', marginTop:'15px' }}>Cart ID: {cartId}</h2>
-      <button
-        onClick={handleCheckout}
-        style={{
-          marginTop: '20px',
-          padding: '15px',
-          fontSize: '16px',
-          backgroundColor: '#e74c3c', // Another color for the button
-          color: '#fff',
-          border: 'none',
-          cursor: 'pointer',
-          borderRadius: '8px',
-        }}
-      >
-        Go to Checkout
-      </button>
-    </div>
-    </>
-  );
+    
+    return (
+        <div>
+            <h1>Checkout test</h1>
+            <CheckoutComponentWithNoSSR />
+        </div>
+    );
 };
 
 export default Home;
